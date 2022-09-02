314568466ec7272f4d236f3f4f6acfa1aa601022
NO1224 <kiyurino16@gmail.com>
2022-09-02 16:12:02 +0900

    コントローラー applicationコメント

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,10 +4,12 @@ class ApplicationController < ActionController::Base
 
   private
 
+  #deviseを介した新規登録時にニックネームを許可する
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
   end
 
+  #環境変数による登録を介したBASIC認証
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
       username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
