96ffdce90e6bb483b67166e9b8d3464d94fb4417
NO1224 <kiyurino16@gmail.com>
2022-09-05 17:14:00 +0900

    サービス種別j表示avascript基盤

--- app/javascript/packs/services.js
+++ app/javascript/packs/services.js
@@ -1,4 +1,12 @@
+// 全ボタン要素を取得
+const buttons = document.querySelectorAll('.service-condition');
 // 押したボタンのid名取得
-document.getElementById('button').addEventListener('click', function(){
-  alert('id名「' + this.id + '」のボタンを押しました。');
-});
+for(let i = 0; i < buttons.length; i++) {
+buttons[i].addEventListener('click', function(){
+  alert('id名「' + i + '」のボタンを押しました。');
+})};
+
+// 押したボタンに応じて内容を切替
+function contentSwitch(){
+
+};
