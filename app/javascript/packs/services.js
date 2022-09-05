f60b964efa99b59aac9e3d3d0da4a49c29a4393b
NO1224 <kiyurino16@gmail.com>
2022-09-05 17:46:42 +0900

    Javascript修正

--- app/javascript/packs/services.js
+++ app/javascript/packs/services.js
@@ -1,12 +1,12 @@
 // 全ボタン要素を取得
-const buttons = document.querySelectorAll('.service-condition');
+const buttons = document.querySelectorAll('#button');
 // 押したボタンのid名取得
 for(let i = 0; i < buttons.length; i++) {
-buttons[i].addEventListener('click', function(){
-  alert('id名「' + i + '」のボタンを押しました。');
-})};
+  buttons[i].addEventListener('click', function(){contentSwitch(i)})
+};
 
 // 押したボタンに応じて内容を切替
-function contentSwitch(){
-
+function contentSwitch(i) {
+  alert('id名「' + i + '」のボタンを押しました。');
+  if (i == 0){}
 };
