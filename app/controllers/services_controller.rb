3b377995eb5c4153b0140f19e918dc1735e5bc61
NO1224 <kiyurino16@gmail.com>
2022-09-10 10:01:56 +0900

    サービス内容切替 javascript修正中

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,6 +1,12 @@
 class ServicesController < ApplicationController
   def index
     @services = ServiceContent.all
+    #javascript使用
+    #if (ボタン1：category:1のみ)
+    #@services = ServiceContent.where(category: 1)
+    #else (ボタン2:category:2のみ)
+    #@services = ServiceContent.where(category: 2)
+
   end
 
 end
