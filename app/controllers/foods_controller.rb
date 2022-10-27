dfdf11ee7c147242118208f13457001d0b3da458
NO1224 <kiyurino16@gmail.com>
2022-10-27 14:44:48 +0900

    その他

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -9,10 +9,8 @@ class FoodsController < ApplicationController
   def create
     @food = Food.new(food_params)
     #inseason_ids = params[:food][:inseason_ids].split(",") 
-    #@food.inseasons_save(inseason_ids)
     if @food.save
       @food_params = params[:inseason_ids]
-      binding.pry
       @food_params.each do |inseason_id|
         #.pluck--カラムの値を配列として取り出すメソッド
         inseason=Season.find(id: inseason_id.to_i)
